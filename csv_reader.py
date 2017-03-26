70e9b310061f928582fb5b007d226cfc3286dd41
Eric Tan <eric.tan@wspdigital.com>
2017-03-26 11:57:24 +1100

    Add csv_reader module

--- csv_reader.py
+++ csv_reader.py
@@ -1,2 +1,23 @@
+"""A pure csv reader which returns a data in rows"""
+
 import csv
 
+
+class NotEnoughDataException(Exception):
+    pass
+
+
+def get_data(file_name):
+    """ Return a nested list containing data of the csv file with file_name
+     which is under the current directory"""
+
+    with open(file_name) as fp:
+        rows = csv.reader(fp)
+
+        data = [row for row in rows]
+
+        # Raise Exception if the file is empty or only has header.
+        if len(data) <= 1:
+            raise NotEnoughDataException
+        else:
+            return data
