8ed3cf3acdff888d74e74070552b268e9b77633c
Eric Tan <eric.tan@wspdigital.com>
2017-03-26 11:59:04 +1100

    Add main module

--- phase1.py
+++ phase1.py
@@ -1,1 +1,17 @@
-import csv
+import csv_reader
+import data_processor
+import xml_writer
+
+INPUT_CSV_FILE_NAME = 'input.csv'
+OUTPUT_XML_FILE_NAME = 'output.xml'
+
+
+def main():
+
+    data = csv_reader.get_data(INPUT_CSV_FILE_NAME)
+    feature_data = data_processor.process_data(data)
+    xml_writer.write_data_to_xml(feature_data)
+
+
+if __name__ == "__main__":
+    main()
