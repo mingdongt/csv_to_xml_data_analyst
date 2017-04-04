aedcda775c8821bdc8a1b161946495093b34a150
Eric Tan <eric.tan@wspdigital.com>
2017-04-04 12:26:06 +1000

    Enable user-defined paths for input and output files

--- phase1.py
+++ phase1.py
@@ -4,16 +4,20 @@ import csv_reader
 import data_analyzer
 import xml_writer
 
-INPUT_CSV_FILE_NAME = 'input.csv'
-OUTPUT_XML_FILE_NAME = 'output.xml'
-
 
 def main():
 
-    data = csv_reader.get_data(INPUT_CSV_FILE_NAME)
-    feature_data = data_analyzer.process_data(data)
-    xml_writer.write_data_to_xml(feature_data, OUTPUT_XML_FILE_NAME)
+    input_file_path = \
+        raw_input("Enter the path of the CSV file that would be processed: ")
+    output_file_path = \
+        raw_input("Enter the path of the XML file that would be generated: ")
 
+    data = csv_reader.get_data(input_file_path)
+    feature_data = data_analyzer.process_data(data)
+
+    xml_writer.write_data_to_xml(feature_data, output_file_path)
+
+    print ("The XML file has been successfully generated.")
 
 if __name__ == "__main__":
     main()
